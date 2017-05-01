479cc34ecfb1c1e5304aa767da434acd4fe67bbc
David Guinane <david.j.guinane@gmail.com>
2017-05-01 16:46:15 +1000

    Updated waive template

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.conf import settings
 from . import views
 from posts.views import PostList, PostDetail, NewPost, PostEdit, HomeView
+from django.conf.urls.static import static
 
 app_name = 'posts'
 urlpatterns = [
@@ -9,4 +10,4 @@ urlpatterns = [
 	url(r'^post/(?P<pk>\d+)/$', PostDetail.as_view(), name="post_detail"),
 	url(r'^post/new/$', NewPost.as_view(), name="new_post"),
 	url(r'^post/(?P<pk>\d+)/edit/$', PostEdit.as_view(), name='post_edit'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- waive/urls.py
+++ waive/urls.py
@@ -5,10 +5,10 @@ from waive.views import (
 	WaiveView,
 	WaiveDataView
 	)
-
+from django.conf.urls.static import static
 
 app_name = 'waive'
 urlpatterns = [
 	url(r'^waive/$', WaiveView.as_view(), name="waive"),
 	url(r'^waive/data$', WaiveDataView.as_view(), name="waive-data"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
