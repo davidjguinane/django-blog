9a0389a90e954975c9ec6946aecbc4ef2039a0e5
David Guinane <david.j.guinane@gmail.com>
2017-05-02 10:06:16 +1000

    Production settings to support S3 and Storages app

--- blog/settings/local.py
+++ blog/settings/local.py
@@ -1,4 +1,4 @@
-from settings.base import *
+from blog.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -6,3 +6,11 @@ DEBUG = True
 INSTALLED_APPS += [
     'django.contrib.staticfiles',
 ]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- blog/settings/production.py
+++ blog/settings/production.py
@@ -1,9 +1,16 @@
-from settings.base import *
+from blog.settings.base import *
 
 DEBUG = False
 
+DEFAULT_FILE_STORAGE = 'aws_storage_classes.MediaStorage'
+STATICFILES_STORAGE = 'aws_storage_classes.StaticStorage'
+AWS_ACCESS_KEY_ID = 'AKIAJM6WYOVTZISQKMHQ'
+SECRET_ACCESS_KEY_ID = 'lDmfhrV0ep3rlUhZ6JVALuuf7f+j3K2LlzD4cWws'
+AWS_STORAGE_BUCKET_NAME = 'xavid.io'
+
+
 INSTALLED_APPS += [
-	'django-storages'
+	'storages'
 ]
 
 ALLOWED_HOSTS += [
@@ -11,3 +18,15 @@ ALLOWED_HOSTS += [
     'xavid.io',
     '13.55.136.6'
 ]
+
+# S3 Bucket URL: <bucket-name>.s3.amazonaws.com
+#STATIC_URL: <bucket-name>.s3.amazonaws.com/static/
+#MEDIA_URL: <bucket-name>.s3.amazonaws.com/media/
+
+AWS_S3_DOMAIN = '{0}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
+
+STATIC_URL = 'https://{0}/static/'.format(AWS_S3_DOMAIN)
+MEDIA_URL = 'https://{0}/media/'.format(AWS_S3_DOMAIN)
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
