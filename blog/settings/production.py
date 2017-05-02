6ea7d327eb61ca0ab89414d2cac6a359aa9693a0
David Guinane <david.j.guinane@gmail.com>
2017-05-02 10:51:01 +1000

    Updated product STATIC_URL and MEDIA_URL

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -23,10 +23,13 @@ ALLOWED_HOSTS += [
 #STATIC_URL: <bucket-name>.s3.amazonaws.com/static/
 #MEDIA_URL: <bucket-name>.s3.amazonaws.com/media/
 
-AWS_S3_DOMAIN = '{0}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
+#AWS_S3_DOMAIN = '{0}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 
-STATIC_URL = 'https://{0}/static/'.format(AWS_S3_DOMAIN)
-MEDIA_URL = 'https://{0}/media/'.format(AWS_S3_DOMAIN)
+#STATIC_URL = 'https://{0}/static/'.format(AWS_S3_DOMAIN)
+#MEDIA_URL = 'https://{0}/media/'.format(AWS_S3_DOMAIN)
+
+STATIC_URL = 'https://xavid.io.s3.amazonaws.com/static/'
+MEDIA_URL = 'https://xavid.io.s3.amazonaws.com/media/'
 
 STATIC_ROOT = '/var/www/django-blog/static/'
 MEDIA_ROOT = '/var/www/django-blog/media/'
